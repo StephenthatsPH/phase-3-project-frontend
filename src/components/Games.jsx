f3ac2afee0b6514f94a87fb360db482e4bd52e4d
Stephen <stephen.a.nerby@gmail.com>
2023-01-26 10:28:36 -0500

    start of update function

--- src/components/Games.jsx
+++ src/components/Games.jsx
@@ -1,18 +1,18 @@
 import React, { useState } from "react";
 
-function Games({ games, handleDelete, handleUpdate }) {
+function Games({ games, handleDelete, selectGame }) {
 
     return (
         <div>
             {games.map((game) => (
-                <div className="game-preview" key={game.id}>
+                <tbody className="game-preview" key={game.id}>
                     <tr hidden>{game.id}</tr>
                     <td>Title: {game.title}</td>
                     <td>Publisher: {game.publisher}</td>
                     <td>Platform: {game.platform}</td><br></br>
-                    <td><button onClick={() => handleUpdate(game.id)}>Update</button></td>
+                    <td><button onClick={() => selectGame(game.id)}>Update</button></td>
                     <td><button onClick={() => handleDelete(game.id)}>Delete</button></td>
-                </div>
+                </tbody>
             ))}
         </div>
     );
--- src/components/GamesList.jsx
+++ src/components/GamesList.jsx
@@ -3,6 +3,9 @@ import Games from './Games';
 
 const GamesList = () => {
     const [games, setGames] = useState(null);
+    const [title, setTitle] = useState("");
+    const [publisher, setPublisher] = useState("");
+    const [platform, setPlatform] = useState("");
     const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
     const [searchChange, setSearchChange] = useState('');
@@ -38,7 +41,11 @@ const GamesList = () => {
                     return res.json();
                 })
                 .then(data => {
-                    setGames(data)
+                    console.log(data)
+                    setGames(data);
+                    setTitle(data.title);
+                    setPlatform(data.platform);
+                    setPublisher(data.publisher);
                     setIsPending(false);
                     setError(null);
                     setRefresh(false);
@@ -52,6 +59,15 @@ const GamesList = () => {
 
     function handleSearchChange(event) {
         setSearchChange(event.target.value);
+    }
+
+    function selectGame(id) {
+        console.warn(games[id - 3])
+        let item = games.id
+        setTitle(item.title);
+        setPlatform(item.platform);
+        setPublisher(item.publisher);
+
     }
 
     return (
@@ -69,9 +85,22 @@ const GamesList = () => {
                 <option>Nintendo</option>
                 <option>Other</option>
             </select>
+            <div className="update-form" style={{ float: "right" }}>
+                <input type="text" value={title} /> <br />
+                <input type="text" value={publisher} /> <br />
+                <select value={platform}>
+                    <option value="" disabled selected hidden>Select Platform</option>
+                    <option value="Playstation">Playstation</option>
+                    <option value="Xbox">Xbox</option>
+                    <option value="PC">PC</option>
+                    <option value="Nintendo Switch">Nintendo Switch</option>
+                    <option value="Other">Other</option>
+                </select> <br />
+                <button>Update Game</button>
+            </div>
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
-            {games && <Games games={games} handleDelete={handleDelete} handleUpdate={handleUpdate} />}
+            {games && <Games games={games} handleDelete={handleDelete} selectGame={selectGame} />}
         </div>
     );
 };
